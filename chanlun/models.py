56a3463974176d96db8d3e6bc083eb94a9c6f378
dogfun <dog@w2w.fun>
2021-10-18 16:27:31 +0800

    add uwsgi

--- chanlun/models.py
+++ chanlun/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 
 # Create your models here.
 class Strategy(models.Model):
@@ -34,3 +34,66 @@ class Order(models.Model):
 
     class Meta:
         db_table = 'cl_order'
+
+    class DbBarData(models.Model):
+        id = models.AutoField(primary_key=True)
+        symbol: str = models.CharField(max_length=255)
+        exchange: str = models.CharField(max_length=255)
+        datetime: datetime = models.DateTimeField()
+        interval: str = models.CharField(max_length=255)
+
+        volume: float = models.FloatField()
+        open_interest: float = models.FloatField()
+        open_price: float = models.FloatField()
+        high_price: float = models.FloatField()
+        low_price: float = models.FloatField()
+        close_price: float = models.FloatField()
+
+        class Meta:
+            db_table = 'dbbardata'
+
+    class DbTickData(models.Model):
+        id = models.AutoField(primary_key=True)
+        symbol: str = models.CharField(max_length=255)
+        exchange: str = models.CharField(max_length=255)
+        datetime: datetime = models.DateTimeField()
+
+        name: str = models.CharField(max_length=255)
+        volume: float = models.FloatField()
+        open_interest: float = models.FloatField()
+        last_price: float = models.FloatField()
+        last_volume: float = models.FloatField()
+        limit_up: float = models.FloatField()
+        limit_down: float = models.FloatField()
+
+        open_price: float = models.FloatField()
+        high_price: float = models.FloatField()
+        low_price: float = models.FloatField()
+        pre_close: float = models.FloatField()
+
+        bid_price_1: float = models.FloatField()
+        bid_price_2: float = models.FloatField(null=True)
+        bid_price_3: float = models.FloatField(null=True)
+        bid_price_4: float = models.FloatField(null=True)
+        bid_price_5: float = models.FloatField(null=True)
+
+        ask_price_1: float = models.FloatField()
+        ask_price_2: float = models.FloatField(null=True)
+        ask_price_3: float = models.FloatField(null=True)
+        ask_price_4: float = models.FloatField(null=True)
+        ask_price_5: float = models.FloatField(null=True)
+
+        bid_volume_1: float = models.FloatField()
+        bid_volume_2: float = models.FloatField(null=True)
+        bid_volume_3: float = models.FloatField(null=True)
+        bid_volume_4: float = models.FloatField(null=True)
+        bid_volume_5: float = models.FloatField(null=True)
+
+        ask_volume_1: float = models.FloatField()
+        ask_volume_2: float = models.FloatField(null=True)
+        ask_volume_3: float = models.FloatField(null=True)
+        ask_volume_4: float = models.FloatField(null=True)
+        ask_volume_5: float = models.FloatField(null=True)
+
+        class Meta:
+            db_table = 'dbtickdata'
